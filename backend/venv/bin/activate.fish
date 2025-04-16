7793ba297e1e1c8a3e11767e7cb093bbbdcc24e3
stanleyndonj <graystanleyy@gmail.com>
2025-04-16 20:35:40 +0300

    nothing new

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/stanley/personal-projects/tradewizard-bazaar/backend/venv
+set -gx VIRTUAL_ENV /home/stanley/tradewizard-bazaar/backend/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
